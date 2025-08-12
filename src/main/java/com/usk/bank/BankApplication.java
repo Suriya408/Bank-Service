b867e7acfa54f889aef7032c94478e25cbac7c67
Suriya408 <suryasowndar@gmail.com>
2025-08-12 13:33:26 +0530

    Update BankApplication.java

--- src/main/java/com/usk/bank/BankApplication.java
+++ src/main/java/com/usk/bank/BankApplication.java
@@ -2,12 +2,9 @@ package com.usk.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class BankApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
 	}
